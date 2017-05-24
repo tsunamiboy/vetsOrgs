118083943ebc28fcb94cece72bf045f2962cfa38
David Blake <dvdblake63@gmail.com>
2017-05-24 13:34:42 -0700

    Succeeded in implementing scrollable list of organizations with drill down to details for each. Test files with bogus data were used. isolated issue of non-scroll ability to problems with the simulator. External device does not have the issue.

--- VetsOrgs/AppDelegate.swift
+++ VetsOrgs/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        return true
+    }
 /*
         var dictOrgs = [String:String]()
         var arrayOrgs = NSMutableArray()
@@ -80,8 +82,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
  */
-        return true
-    }
+
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- VetsOrgs/ViewController.swift
+++ VetsOrgs/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+    
+    var funcSectCount: Int = 0
+    var funcRowCount: Int = 0
+    
     @IBOutlet weak var myTable: UITableView!
 //    comment
         override func viewDidLoad() {
@@ -19,15 +22,18 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
         func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 //            let data = AppData.items
+            funcSectCount += 1
             let data = AppData.orgNames
-            print("data.count =", data.count)
+            print("data.count=", data.count, "  func Section count=", funcSectCount)
             return data.count
         }
         func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 //            let data = AppData.items
+            funcRowCount += 1
             let data = AppData.orgNames
             let cell = myTable.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
             cell.textLabel?.text = data[indexPath.row]
+            print("indexPath.row=", indexPath.row, "  func Row count=", funcRowCount)
             return cell
         }
         func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
